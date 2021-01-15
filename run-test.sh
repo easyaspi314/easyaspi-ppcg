3d50f4d0dd1cc4e3ec0f3ead484ed70103cbe45b
easyaspi314 <easyaspi314@users.noreply.github.com>
2021-01-14 19:20:09 -0500

    Add command folding

--- run-test.sh
+++ run-test.sh
@@ -26,33 +26,46 @@ mips-*)
     ;;
 esac
 
-run_executable()
+print_and_run()
 {
-    [ -n "$STDIN" ] && printf "STDIN: '%s'\n" "$STDIN"
-    printf "Command line args: ./a.out "
+    printf "$ "
     for i in $@; do
         printf "\"%s\" " "$i"
     done
     printf "\n"
-    eval $QEMU ./a.out $@
+    eval $@
+}
+
+run_executable()
+{
+    [ -n "$STDIN" ] && printf "STDIN: '%s'\n" "$STDIN"
+    printf "Command line args: "
+    printf "%s" "$STDIN" | print_and_run $QEMU ./a.out $@
+
     echo "Return code: $?"
 }
-echo "Installing dependencies..." 
-sudo apt-get update -y -qq >/dev/null
-sudo apt-get install -y -qq gcc-$TRIPLE clang qemu-user-static libc6-dev-$LIBC-cross >/dev/null
 
-set -x
+
+echo "Installing dependencies..." 
+travis_fold start "runtest.apt"
+print_and_run sudo apt-get update
+print_and_run sudo apt-get install -y gcc-$TRIPLE clang qemu-user-static libc6-dev-$LIBC-cross
+travis_fold end "runtest.apt"
 
 cd $1
 echo "Source code:"
-cat file.S
+travis_fold start "sourcecode"
+print_and_run cat file.S
+travis_fold end "sourcecode"
 
 # I prefer Clang's assembler, it is a little more flexible than GAS.
-clang --target=$TRIPLE -c file.S -o file.o
+print_and_run clang --target=$TRIPLE -c file.S -o file.o
 echo "Object file info:"
+travis_fold start "objinfo"
 # Show details about the object file
-$TRIPLE-size -A file.o
-$TRIPLE-objdump -d file.o
+print_and_run $TRIPLE-size -A file.o
+print_and_run $TRIPLE-objdump -d file.o
+travis_fold end "objinfo"
 echo "Compiling test suite"
 
 LDFLAGS=
@@ -63,10 +76,10 @@ if [ $($TRIPLE-nm -g file.o | grep -s _start) ]; then
 fi
 
 # Link file.o, optionally with a driver.c or driver.S if it exists.
-$TRIPLE-gcc -static $LDFLAGS $(compgen -G 'driver.[cS]' || true) file.o -o a.out
+print_and_run $TRIPLE-gcc -static $LDFLAGS $(compgen -G 'driver.[cS]' || true) file.o -o a.out
 
 # Disable error failing and command printing
-set +xe
+set +e
 
 echo Running tests...
 # Run the tests.
