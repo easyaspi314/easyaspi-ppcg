1104150055db685524d77bb02e51f7a919362b0e
easyaspi314 <easyaspi314@users.noreply.github.com>
2021-01-14 19:31:25 -0500

    Better folding

--- run-test.sh
+++ run-test.sh
@@ -28,44 +28,41 @@ esac
 
 print_and_run()
 {
-    printf "$ "
-    for i in $@; do
-        printf "\"%s\" " "$i"
-    done
-    printf "\n"
+    echo "$@"
     eval $@
 }
 
 run_executable()
 {
     [ -n "$STDIN" ] && printf "STDIN: '%s'\n" "$STDIN"
-    printf "Command line args: "
     printf "%s" "$STDIN" | print_and_run $QEMU ./a.out $@
 
     echo "Return code: $?"
 }
 
-
-echo "Installing dependencies..." 
+ 
 travis_fold start "runtest.apt"
+echo "Installing dependencies..."
 print_and_run sudo apt-get update
 print_and_run sudo apt-get install -y gcc-$TRIPLE clang qemu-user-static libc6-dev-$LIBC-cross
 travis_fold end "runtest.apt"
 
 cd $1
-echo "Source code:"
 travis_fold start "sourcecode"
+echo "Source code:"
 print_and_run cat file.S
 travis_fold end "sourcecode"
 
 # I prefer Clang's assembler, it is a little more flexible than GAS.
 print_and_run clang --target=$TRIPLE -c file.S -o file.o
-echo "Object file info:"
+
 travis_fold start "objinfo"
+echo "Object file info:"
 # Show details about the object file
 print_and_run $TRIPLE-size -A file.o
 print_and_run $TRIPLE-objdump -d file.o
 travis_fold end "objinfo"
+
 echo "Compiling test suite"
 
 LDFLAGS=
@@ -76,10 +73,10 @@ if [ $($TRIPLE-nm -g file.o | grep -s _start) ]; then
 fi
 
 # Link file.o, optionally with a driver.c or driver.S if it exists.
-print_and_run $TRIPLE-gcc -static $LDFLAGS $(compgen -G 'driver.[cS]' || true) file.o -o a.out
+$TRIPLE-gcc -static $LDFLAGS $(compgen -G 'driver.[cS]' || true) file.o -o a.out
 
 # Disable error failing and command printing
-set +e
+set +xe
 
 echo Running tests...
 # Run the tests.
