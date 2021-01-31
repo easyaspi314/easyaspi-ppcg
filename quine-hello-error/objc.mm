5d39ce65042a69ac99396f76f09aa71313444cb8
easyaspi314 (Devin) <easyaspi314@users.noreply.github.com>
2021-01-30 21:17:50 -0500

    quine-error-hello: -1 byte: remove space after .print

--- quine-hello-error/objc.mm
+++ quine-hello-error/objc.mm
@@ -1,2 +1,2 @@
 @import std;int main(){auto s="@import std;int main(){auto s=%c%s%1$c;printf(s,34,s,R%1$c(%3$s)%1$c,10);}%4$c";printf(s,34,s,R"(
-.macro instruction x;.print "Hello, World!";.endm@)",10);}
+.macro instruction x;.print"Hello, World!";.endm@)",10);}
